b347a05e2d4b421809b8fbb200a8014029239a7d
Corey <ca.chao3@gmail.com>
2014-07-09 22:02:14 +0000

    Much belated push of homework assignments...

---
+++ Fizzbuzz/fizzbuzz.js.js
@@ -0,0 +1,14 @@
+var soda = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+for (j=0; j < soda.length; j++) {
+    
+    if (soda[j] % 3 = 0 && soda[j] % 5 = 0) {
+        console.log("fizzbuzz")
+    } else if (soda[j] % 3 != 0 && soda[j] % 5 = 0) {
+        console.log("buzz")
+    } else if (soda[j] % 3 = 0 && soda[j] % 5 != 0) {
+        console.log("fizz")
+    } else {
+        console.log(soda[j]
+    }
+}
